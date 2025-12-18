08443aceb85aca54f2aa7fa85f7518c4f337a3d4
Dmitry Marakasov <amdmi3@FreeBSD.org>
2025-12-18 14:28:03 +0300

    (WIP) games/irrlamb: update 1.0.6 → 1.0.7

--- games/irrlamb/files/patch-cmake_version.cmake
+++ games/irrlamb/files/patch-cmake_version.cmake
@@ -1,14 +1,11 @@
---- cmake/version.cmake.orig	2022-09-29 21:46:35 UTC
+--- cmake/version.cmake.orig	2025-02-20 03:02:25 UTC
 +++ cmake/version.cmake
-@@ -1,9 +1,7 @@
--execute_process(COMMAND git rev-parse --short HEAD RESULT_VARIABLE RETURN_CODE OUTPUT_VARIABLE BUILD_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-+set(BUILD_VERSION 0)
- 
--if(RETURN_CODE EQUAL 0)
- 	configure_file(
- 		"${SRC}"
- 		"${DST}"
- 		@ONLY
- 	)
--endif()
-\ No newline at end of file
+@@ -2,7 +2,7 @@ execute_process(
+ # Attempt to get build version from git
+ execute_process(
+ 	COMMAND
+-		git rev-parse --short HEAD
++		echo 0
+ 	RESULT_VARIABLE
+ 		RETURN_CODE
+ 	OUTPUT_VARIABLE
