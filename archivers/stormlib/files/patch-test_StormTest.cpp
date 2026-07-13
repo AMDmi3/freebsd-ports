6cd5c415d524ae9aaac76364000af17b07a20009
Dmitry Marakasov <amdmi3@FreeBSD.org>
2026-07-13 22:04:26 +0300

    (WIP: breaks both dependees) archivers/stormlib: update 9.31 → 9.40

---
+++ archivers/stormlib/files/patch-test_StormTest.cpp
@@ -0,0 +1,11 @@
+--- test/StormTest.cpp.orig	2026-07-04 12:21:58 UTC
++++ test/StormTest.cpp
+@@ -2493,7 +2493,7 @@ static DWORD TestOpenArchive_SearchEmbedded(TLogHelper
+     else
+     {
+         Logger.PrintError("Failed to search the archive");
+-        dwErrCode = GetLastError();
++        dwErrCode = SErrGetLastError();
+     }
+     return dwErrCode;
+ }
